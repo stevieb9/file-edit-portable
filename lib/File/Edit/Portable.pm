2e48bb61b34964686703342576c820f416f883d9
Steve Bertrand <steveb@cpan.org>
2016-01-23 09:35:06 -0700

    fixes #17; bugracker link fixed. Bumped ver of Mock::Sub to 1.06

--- lib/File/Edit/Portable.pm
+++ lib/File/Edit/Portable.pm
@@ -582,9 +582,7 @@ Steve Bertrand, C<< <steveb at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-file-edit-portable at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=File-Edit-Portable>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests to L<https://github.com/stevieb9/mock-sub/issues>
 
 =head1 REPOSITORY
 
